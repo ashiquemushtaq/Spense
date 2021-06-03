23d8c0edeb2b056df3e13eb75c44571478c71395
Ashique Mushtaq <ashiquemushtaqt@gmail.com>
2021-06-03 10:41:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const ham = document.getElementById("ham");
-const nav = document.getElementById("nav");
+const nav = document.getElementById("nav-bar");
 
 ham.addEventListener("click",()=>{
     nav.classList.toggle("show");
